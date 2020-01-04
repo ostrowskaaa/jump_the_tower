fb43b634d10b97193d59f8530c2794058f831ec9
ostrowskaaa <50330163+ostrowskaaa@users.noreply.github.com>
2020-01-04 23:55:20 +0100

    display score

--- Tower.py
+++ Tower.py
@@ -5,6 +5,24 @@ from Platform import Platform
 from Platform_controller import PlatformController
 from pygame.locals import *
 
+#text
+def text_objects(text, font, color):
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
+
+def message_display(window, text, x, y, font_size, color, centered_x=False, centered_y=False):
+    font = pygame.font.Font(None,font_size)
+    TextSurf, TextRect = text_objects(text, font, color)
+    if centered_x and centered_y:
+    	TextRect.center = ((W/2),(H/2))
+    elif centered_x:
+    	TextRect.center = ((W/2),y)
+    elif centered_y:
+    	TextRect.center = (x,(H/2))
+    else:
+    	TextRect.center = (x,y)
+    window.blit(TextSurf, TextRect)
+
 #colors
 background = (123, 174, 163)
 black = (0,0,0)
@@ -61,7 +79,10 @@ def game_over():
     window = pygame.display.set_mode((W, H))
     window.fill(black)
     game_over_img = pygame.image.load('game-over.png')
-    window.blit(game_over_img, (W/2 - 64, H/2 - 64))
+    window.blit(game_over_img, (W/2 - 64, H/2 - 160))
+    message_display(window, "Score: %d" % player.score, 0, 300, 50, white, True)
+    message_display(window, "Press SPACE to play again!", 0, 370, 50, white, True)
+    message_display(window, "Press ESC to quit!", 0, 440, 40, white, True)
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -88,6 +109,7 @@ while game:
         floor.draw(window, camera)
         platform_controller.draw(window, camera)
         player.draw(window, camera)
+        message_display(window, str(player.score), 25, 30, 36, white)
 
         if player.fallen_off_screen(camera) == True:
             game_over()
